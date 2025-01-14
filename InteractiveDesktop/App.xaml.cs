f2d6af37e773e682bc60c0c1c4669a344b482c5e
Antoine <antoine.berger@epitech.eu>
2025-01-14 23:57:07 +0100

    feat(app): implementing tray system

--- InteractiveDesktop/App.xaml.cs
+++ InteractiveDesktop/App.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using H.NotifyIcon;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -32,7 +20,6 @@ namespace Interactive_Desktop
         /// </summary>
         public App()
         {
-            Injecter.Injecter.Inject("", "");
             this.InitializeComponent();
         }
 
@@ -40,12 +27,38 @@ namespace Interactive_Desktop
         /// Invoked when the application is launched.
         /// </summary>
         /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        protected override async void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            m_window = new MainWindow();
-            m_window.Activate();
+            _mWindow = new MainWindow();
+            var appWindow = GetAppWindowForCurrentWindow(_mWindow);
+            {
+                var presenter = appWindow.Presenter as OverlappedPresenter;
+                presenter!.IsAlwaysOnTop = true;
+
+                appWindow.Closing += AppWindow_Closing;
+                _mWindow.Hide();
+            }
+            _mWindow.Activate();
+
+            //var injecter = new Injecter.Injecter(
+            //    WinRT.Interop.WindowNative.GetWindowHandle(_mWindow)
+            //);
+            //injecter.Attach();
         }
 
-        private Window? m_window;
+        private static void AppWindow_Closing(AppWindow sender, AppWindowClosingEventArgs args)
+        {
+            sender.Hide();
+            args.Cancel = true;
+        }
+
+        private static AppWindow GetAppWindowForCurrentWindow(Window window)
+        {
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+            var myWndId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(myWndId);
+        }
+
+        private Window? _mWindow;
     }
 }
